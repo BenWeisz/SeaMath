1e53434d73932c1a01bd458a83c8e5960d5814a7
benweisz <ben.weisz@mail.utoronto.ca>
2022-11-26 23:56:19 -0500

    Add timing experiment

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "seamath.h"
 
 int main() {
     init_seamath();
 
-    int N = 1000000;
+    int N = 10000000;
 
     Vector* v1 = random_vector(N, 0, 10);
     Vector* v2 = random_vector(N, 0, 10);
 
+    clock_t start, end;
+
+    start = clock();
     float r = dot(v1, v2);
+    end = clock();
 
-    printf("%f\n", r);
+    double time_taken = (double)(end - start);
 
+    printf("%f\n", time_taken);
+
+    start = clock();
     float res = 0.0;
     for (int i = 0; i < N; i++)
         res += v1->data[i] * v2->data[i];
+    end = clock();
 
-    printf("%f\n", res);
+    time_taken = (double)(end - start);
+
+    printf("%f\n", time_taken);
 
     free_vector(v1);
     free_vector(v2);
